e8153a0757620439c6b9bc68b544305a3ec2d849
Diego <diego.caudillo@uabc.edu.mx>
2021-02-05 23:34:25 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const nodemailer = require('nodemailer');
+const bodyParser = require("body-parser");
+const app = express();
+
+const RECEIVER_EMAIL = "diego.caudillo@uabc.edu.mx"
+const PASSWORD = "golied12"
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname + "/index.html"))
+})
+
+app.post('/sendEmail', (req, res)=>{
+    comment_info = req.body;
+    
+    const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+        user: RECEIVER_EMAIL,
+        pass: PASSWORD
+    }
+    });
+
+    const mailOptions = {
+        from: RECEIVER_EMAIL,
+        to: comment_info.email,
+        subject: 'Portfolio comment',
+        text: comment_info.comment
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+    res.redirect('/');
+});
+})
+app.listen(5000, ()=> console.log("App initialized on port 5000"));
